d0add32cc0b9a705f5b09685ffe0a08004cb2003
jim-at-jibba <jim@justjibba.net>
2019-09-09 21:56:50 +0100

    Ready for amplify

--- App.js
+++ App.js
@@ -3,46 +3,66 @@ import { StatusBar, View } from 'react-native';
 import { AppLoading, ScreenOrientation } from 'expo';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { device, func, gStyle } from './src/constants';
+// import Amplify, { Auth } from 'aws-amplify';
+// import awsmobile from './aws-exports';
+// import { ApolloProvider } from 'react-apollo';
+// import { Rehydrated } from 'aws-appsync-react';
+// import AWSAppSyncClient from 'aws-appsync';
+
 // navigation switch
 import AppSwitchNav from './src/navigation/AppSwitchNav';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+// Amplify.configure(awsmobile);
 
-    this.state = {
-      isLoading: true
-    };
+// const client = new AWSAppSyncClient({
+//   url: awsmobile.aws_appsync_graphqlEndpoint,
+//   region: awsmobile.aws_appsync_region,
+//   disableOffline: true,
+//   auth: {
+//     type: awsmobile.aws_appsync_authenticationType,
+//     credentials: () => Auth.currentCredentials(),
+//     jwtToken: async () =>
+//       (await Auth.currentSession()).getAccessToken().getJwtToken()
+//   },
+//   complexObjectsCredentials: () => Auth.currentCredentials()
+// });
 
-    // iPad? (TODO in future android tablet checked)
-    if (device.isTablet) {
-      ScreenOrientation.allowAsync(
-        ScreenOrientation.Orientation.ALL_BUT_UPSIDE_DOWN
-      );
-    }
-  }
+const App = () => {
+  const [isLoading, setLoading] = React.useState(true);
 
-  render() {
-    const { isLoading } = this.state;
-
-    if (isLoading) {
-      return (
-        <AppLoading
-          onFinish={() => this.setState({ isLoading: false })}
-          startAsync={func.loadAssetsAsync}
-        />
-      );
-    }
-
+  if (isLoading) {
     return (
-      <View style={gStyle.container}>
-        <StatusBar barStyle={device.iOS ? 'dark-content' : 'light-content'} />
-        <PaperProvider>
-          <AppSwitchNav />
-        </PaperProvider>
-      </View>
+      <AppLoading
+        onFinish={() => setLoading(false)}
+        startAsync={func.loadAssetsAsync}
+      />
     );
   }
-}
+
+  return (
+    <View style={gStyle.container}>
+      <StatusBar barStyle={device.iOS ? 'dark-content' : 'light-content'} />
+      <PaperProvider>
+        <AppSwitchNav />
+      </PaperProvider>
+    </View>
+  );
+};
+
+// <ApolloProvider client={client}>
+//   <Rehydrated>
+//     <PaperProvider>
+//       <View style={gStyle.container}>
+//         <StatusBar
+//           barStyle={
+//             device.OS === 'ios' ? 'dark-content' : 'light-content'
+//           }
+//         />
+
+//         <AppSwitchNav />
+//       </View>
+//     </PaperProvider>
+//   </Rehydrated>
+// </ApolloProvider>
 
 export default App;
